1c45c13086a7d923bdfe10c6d89dfef2014a891c
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-08-04 10:50:21 +0700

    Create Convertyear.py

---
+++ Convertyear.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title ("แปลง พ.ศ. เป็น ค.ศ.")
+
+bh_year =  st.number_input("กรอกปี พ.ศ. ที่ต้องการแปลง", value = 2569)
+ch_year = bhyear-543
+st.header(f"ปี ค.ศ. คือ ("{ch_year}")
