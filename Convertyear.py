00cc553dbbdf42bdbd5729d76a60fe8170795909
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-08-04 10:56:58 +0700

    Update Convertyear.py

--- Convertyear.py
+++ Convertyear.py
@@ -3,4 +3,4 @@ st.title ("แปลง พ.ศ. เป็น ค.ศ.")
 
 bh_year =  st.number_input("กรอกปี พ.ศ. ที่ต้องการแปลง", value = 2569)
 ch_year = bh_year-543
-st.header(f"ปี ค.ศ. คือ ("{ch_year}")
+st.header(f"ปี ค.ศ. คือ : {ch_year} ")
