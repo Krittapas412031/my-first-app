e52906f888402ece25f3f6fc28999680a35fd164
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-08-11 09:17:46 +0700

    Update vat7.py

--- vat7.py
+++ vat7.py
@@ -1,8 +1,9 @@
-st.title("แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-vat = price * 0.07
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท") 
-import streamlit as st 
-price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0) 
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07 
 net_price = price - vat
-st.write("นายกฤตภาส สายทอง เลขที่ 36  ม.4/12")
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider() 
+st.write("นางกฤตภาส สายทอง เลขที่ 36  ม.4/12")
