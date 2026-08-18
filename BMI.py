d25f343cc0b402b9572833701ddc34965fe65bcb
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-08-18 10:33:17 +0700

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,34 +1,28 @@
 import streamlit as st
 
-#1 ส่วนหน้าเว็บ
-st. markdown("#: red [ค่าคำนวนดัชนีมวลกาย BMI]")
-st.write ("กรอกข้อมูลและน้ำหนักของคุณเพื่อเช็คสุขภาพเบื้องต้น") 
+print("=== BMI Calculator ===")
 
-#2 ช่องรับข้อมูลน้ำหนักและส่วนสูง
-weight = st.number_input("กรอกน้ำหนักของคุณด้วยกิโลกรัม:", min_value = 1.0, value = 1.0) 
-height_cm = st.number_input("กรอกส่วนสูงของคุณด้วยเซนติเมตร:", min_value = 1.0, value = 1.0) 
+weight = float (input("Enter your weight (kg): "))
+height = float(input("Enter your height (cm): ")) 
 
-#3 ปุ่มกดคำณวน 
-if st.button("คำณวนค่า BMI"):
+#Convert centimeters - metres
+height_m = height / 100
 
-  #แปลง cm เป็น m เพื่อหาค่า BMI 
-  height_m = height_cm / 100 
-BMI = weight / (height_m ** 2) 
+#calculate 
+BMI = weight / )height_**2) 
 
-st.write ("---") 
-st.header(f"ค่า BMI ของคุณคือ: **{BMI:.2f}**") 
+print(f"\nYour BMI is: {BMI:.2f}") 
 
-#4 แปลงค่า BMI ตามเกณท์ 
-  if BMI < 18.5:
-    st.warning("ผอมเกินไป")
- elif 18.5 <= BMI < 23.0:
-    st.success ("ผอมพอดีตามเกณฑ์")
-
- elif 23.0 <=BMI < 25.0:
-    st.info ("ท้วมมากกว่าเกณฑ์นิดหน่อย") 
-
- else:
-    st.error ("อ้วนมากกกกกกกกกก ควรรักษาสุขภาพและออกกำลังกาย")
+# classification 
+if BMI <= 18.5 < 25:
+  category = "underweight"
+elif BMI <= 25 < 30:
+  category = "Normal weight" 
+elif BMI <= 30:
+  category = "Overweight"
+else:
+  category = "Obesity"
+  print(f"Category: {category}") 
 
 st.divider() 
 st.write("นายกฤตภาส สายทอง ม.4/12 เลขที่ 36")
