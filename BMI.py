9249c21e358c2e945413cbe62b5b3c9082fcf86a
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-08-18 11:07:32 +0700

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -25,7 +25,7 @@ if st.button("Calculate."):
     elif bmi <= 30:
       st.info("Overweight (a little bit fat)")
     else:
-      st.error("Obesity") 
+      st.info("Obesity") 
 
 
 st.divider() 
