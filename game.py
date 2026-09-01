41a00dee6d8062317ad8a849e5c1ddbc3a073757
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-09-01 10:53:03 +0700

    Update game.py

--- game.py
+++ game.py
@@ -7,9 +7,9 @@ if "ans1_val" not in st.session_state:
    st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
   st.session_state.ans2_val = ""
-  if "ans3_val" not in st.session_state:
+if "ans3_val" not in st.session_state:
   st.session_state.ans1_val = ""
-  if "ans4_val" not in st.session_state:
+if "ans4_val" not in st.session_state:
   st.session_state.ans2_val = ""
 
 # Clear button 
