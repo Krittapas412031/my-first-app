07029daaf6512bd1835a4608ddc97ee25b428527
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-09-01 11:01:01 +0700

    Update game.py

--- game.py
+++ game.py
@@ -109,7 +109,7 @@ st.session_state.ans4_val = ans4
 
 # 4. Enter button
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
-if st.button("📥 Enter"):
+   if st.button("📥 Enter"):
    st.session_state.is_ended = True
    st.rerun()
    time.sleep(1)
