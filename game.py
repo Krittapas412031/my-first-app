9891d0940882239e87acf2d88445f0bdf3355bc6
KRITTAPAS SAITHONG <56722@yupparaj.ac.th>
2026-09-01 10:54:42 +0700

    Update game.py

--- game.py
+++ game.py
@@ -38,25 +38,27 @@ u_ans4 = ans4.strip().lower()
 
 # line 1 check
 if u_ans1 == "apple":
-st.success("✅ ขอ้ 1: Correct")
+   st.success("✅ ขอ้ 1: Correct")
 score += 1
 else:
 st.error(f"❌ ขอ้ 1: Wrong (you use '{u_ans1}')")
+
 # line 2 check
 if u_ans2 == "bone":
-st.success("✅ ขอ้ 2: Correct")
+   st.success("✅ ขอ้ 2: Correct")
 score += 1
 else:
 st.error(f"❌ ขอ้ 2: Wrong (you use '{u_ans2}')")
+
 # student custom question 
 if u_ans3 == "doctor":
-st.success("✅ ขอ้ 1: Correct")
+   st.success("✅ ขอ้ 1: Correct")
 score += 1
 else:
 st.error(f"❌ ขอ้ 3: Wrong (you use '{u_ans3}')")
 # line 2 check
 if u_ans4 == "student":
-st.success("✅ ขอ้ 4: Correct")
+   st.success("✅ ขอ้ 4: Correct")
 score += 1
 else:
 st.error(f"❌ ขอ้ 2: Wrong (you use '{u_ans4}')")
